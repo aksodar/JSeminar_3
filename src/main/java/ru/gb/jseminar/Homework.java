fac19cb78bd516b98cf37efa9fa59f6368e76e90
viktornazilin <104361827+viktornazilin@users.noreply.github.com>
2022-10-22 11:59:38 +0200

    Add files via upload

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -2,35 +2,100 @@ package ru.gb.jseminar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Homework {
 
     // Пусть дан произвольный список целых чисел
     public static void main(String[] args) {
 
+        List<Integer> myList = createRandomList(5);
+        for(int i = 0 ; i < myList.size(); i++){
+            System.out.println(myList.get(i));
+        } 
+
+        removeNegativeValue(myList);
+
+        myList = createRandomList(5);
+        for(int i = 0 ; i < myList.size(); i++){
+            System.out.println(myList.get(i));
+        } 
+        getMin(myList);
+
+        getMax(myList);
+
+        getAverage(myList);
+
     }
 
-    // Нужно удалить из него четные числа
-    public List<Integer> removeNegativeValue(List<Integer> list){
+    public static ArrayList<Integer> createRandomList(int sizeParameter) {
 
-        return new ArrayList<>();
+    ArrayList<Integer> setIntegerList = new ArrayList<Integer>(sizeParameter);
+
+    Random randomHelper = new Random();
+    
+    for (int x = 0; x < sizeParameter; x++)
+    {
+        setIntegerList.add(randomHelper.nextInt(10));
+    }  
+    
+    return setIntegerList;
+}
+
+    // Нужно удалить из него четные числа
+    public static List<Integer> removeNegativeValue(List<Integer> list){
+
+        for (int i = 0; i < list.size(); i++){ 
+                
+            int even = list.get(i) % 2; 
+
+             if (even == 0) {
+                 list.remove(i);
+             }    
+         }
+
+         System.out.println("Список без четных чисел: " + list);
+
+        return list;
     }
 
     // Найти минимальное значение
-    public Integer getMin(List<Integer> list){
+    public static Integer getMin(List<Integer> list){
 
-        return 0;
+        int min = list.get(0);
+        
+        for (Integer i: list) {
+            if(i < min) 
+               min = i;
+        }
+        System.out.println("минимальное число: " + min);
+        return min;
     }
 
     // Найти максимальное значение
-    public Integer getMax(List<Integer> list){
+    public static Integer getMax(List<Integer> list){
 
-        return 0;
+        int max = list.get(0);
+
+        for (Integer i: list) {
+            if(i > max) 
+               max = i;
+        }
+
+        System.out.println("максимальное число: " + max);
+        return max;
     }
 
     // Найти среднее значение
-    public Integer getAverage(List<Integer> list){
+    public static int getAverage(List<Integer> list){
+        int sum = 0;
+        for (int i = 0; i < list.size(); i++){
+            sum += list.get(i);
+        }
+        
+        int average = sum / list.size();
 
-        return 0;
+         System.out.println("Среднее значение: " + average);
+         return average;
     }
 }
