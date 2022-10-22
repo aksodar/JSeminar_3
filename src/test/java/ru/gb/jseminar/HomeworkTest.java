eb8e21b604e131cf910f0504871da460d06e1d3f
viktornazilin <104361827+viktornazilin@users.noreply.github.com>
2022-10-22 11:59:59 +0200

    Add files via upload

--- src/test/java/ru/gb/jseminar/HomeworkTest.java
+++ src/test/java/ru/gb/jseminar/HomeworkTest.java
@@ -1,5 +1,69 @@
 package ru.gb.jseminar;
 
-class HomeworkTest {
+import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class HomeworkTest {
+
+    @Test
+    public void testGetAverage() {
+       //что-то исходное 
+       List<Integer> expectedList = new ArrayList<>(Arrays.asList(1,2,3,4));
+       int expected = 2;
+
+       // что-то делаем
+       Homework homework = new Homework();
+       int actual = homework.getAverage(expectedList);
+
+       // проверяем
+       assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetMax() {
+       //что-то исходное 
+       List<Integer> expectedList = new ArrayList<>(Arrays.asList(1,2,3,4));
+       int expected = 4;
+
+       // что-то делаем
+       Homework homework = new Homework();
+       int actual = homework.getMax(expectedList);
+
+       // проверяем
+       assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetMin() {
+       //что-то исходное 
+       List<Integer> expectedList = new ArrayList<>(Arrays.asList(1,2,3,4));
+       int expected = 1;
+
+       // что-то делаем
+       Homework homework = new Homework();
+       int actual = homework.getMin(expectedList);
+
+       // проверяем
+       assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemoveNegativeValue() {
+       //что-то исходное 
+       List<Integer> expectedList = new ArrayList<>(Arrays.asList(1,2,3,4));
+       List<Integer> expected = new ArrayList<>(Arrays.asList(1,3));
+
+       // что-то делаем
+       Homework homework = new Homework();
+       List<Integer> actual = homework.removeNegativeValue(expectedList);
+
+       // проверяем
+       assertEquals(expected, actual);
+    }
+    
 }
