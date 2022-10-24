ac80949e02818c234664a930a7e06f77ab13aed1
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-10-24 20:46:36 +0300

    add Homework

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,36 +1,67 @@
 package ru.gb.jseminar;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class Homework {
 
 	// Пусть дан произвольный список целых чисел
 	public static void main(String[] args) {
+		System.out.print("\033[H\033[2J");	// Очистка консоли (работает в Windows, на остальных системах не проверял)
+		System.out.flush();
 
+		List<Integer> list = new ArrayList<>(Arrays.asList(1, 9, 2, 6, 4, 3, 5, 7, 8, 0));
+		Homework homework = new Homework();
+
+		Logger log = Logger.getLogger(Homework.class.getName());
+		log.info("Исходный список: " + String.valueOf(list));
+		log.info(String.valueOf("Список нечётных элементов: " + homework.removeNegativeValue(list)));
+		log.info(String.valueOf("Минимальный элемент в списке: " + homework.getMin(list)));
+		log.info(String.valueOf("Максимальный элемент в списке: " + homework.getMax(list)));
+		log.info(String.valueOf("Среднее значение элементов списка: " + homework.getAverage(list)));
 	}
 
 	// Нужно удалить из него четные числа
-	public List<Integer> removeNegativeValue(List<Integer> list){
+	public List<Integer> removeNegativeValue(List<Integer> list) {
+		List<Integer> result = new ArrayList<>(list);
+		Iterator<Integer> item = result.iterator();
 
-		return new ArrayList<>();
+		while (item.hasNext())
+			if (item.next() % 2 == 0)	item.remove();
+		return result;
 	}
 
 	// Найти минимальное значение
-	public Integer getMin(List<Integer> list){
-
-		return 0;
+	public Integer getMin(List<Integer> list) {
+		if (list.size() > 0)	return Collections.min(list);
+		else					throw new IllegalStateException("Невозможно найти минимальный элемент - список пуст!");
 	}
 
 	// Найти максимальное значение
-	public Integer getMax(List<Integer> list){
-
-		return 0;
+	public Integer getMax(List<Integer> list) {
+		if (list.size() > 0) {
+			int max = list.get(0);
+			for (int item : list.subList(1, list.size()))
+				if (item > max)	max = item;
+			return max;
+		} else {
+			throw new IllegalStateException("Невозможно найти максимальный элемент - список пуст!");
+		}
 	}
 
 	// Найти среднее значение
-	public Integer getAverage(List<Integer> list){
-		
-		return 0;
+	public Integer getAverage(List<Integer> list) {
+		if (list.size() > 0) {
+			int avg = 0;
+			for (int item : list)	avg += item;
+			avg /= list.size();
+			return avg;
+		} else {
+			throw new IllegalStateException("Невозможно найти среднее значение - список пуст");
+		}
 	}
 }
--- src/test/java/ru/gb/jseminar/HomeworkTest.java
+++ src/test/java/ru/gb/jseminar/HomeworkTest.java
@@ -1,5 +1,56 @@
 package ru.gb.jseminar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class HomeworkTest {
+
+	@Test
+	void removeNegativeValueSuccessful() {
+		List<Integer> inputList = new ArrayList<>(Arrays.asList(1, 9, 2, 6, 4, 3, 5, 7, 8, 0));
+		List<Integer> expectedList = new ArrayList<>(Arrays.asList(1, 9, 3, 5, 7));
+
+		Homework homework = new Homework();
+		List<Integer> actualList = homework.removeNegativeValue(inputList);
+
+		Assertions.assertEquals(expectedList, actualList);
+	}
+
+	@Test
+	void getMinSuccessful() {
+		List<Integer> inputList = new ArrayList<>(Arrays.asList(9, 2, 6, 4, 3, 5, 7, 8));
+		int expected = 2;
+
+		Homework homework = new Homework();
+		int actual = homework.getMin(inputList);
+
+		Assertions.assertEquals(expected, actual);
+	}
+
+	@Test
+	void getMaxSuccessful() {
+		List<Integer> inputList = new ArrayList<>(Arrays.asList(9, 2, 6, 4, 3, 5, 7, 8));
+		int expected = 9;
+
+		Homework homework = new Homework();
+		int actual = homework.getMax(inputList);
+
+		Assertions.assertEquals(expected, actual);
+	}
+
+	@Test
+	void getAverageSuccessful() {
+		List<Integer> inputList = new ArrayList<>(Arrays.asList(1, 9, 3, 5, 7));
+		int expected = 5;
+
+		Homework homework = new Homework();
+		int actual = homework.getAverage(inputList);
+
+		Assertions.assertEquals(expected, actual);
+	}
 
 }
