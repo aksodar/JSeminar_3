147957d5c499c04b951ab9e46e8521e3be6260cb
Alisa <Alica1200@gmail.com>
2022-10-25 13:57:32 +0300

    HM 3 - Alisa Syaribzhanova

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -2,35 +2,57 @@ package ru.gb.jseminar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 public class Homework {
 
     // Пусть дан произвольный список целых чисел
     public static void main(String[] args) {
+        Homework hw = new Homework();
 
+        ArrayList<Integer> num = new ArrayList<Integer>();
+        num.add(543);
+        num.add(111112);
+        num.add(124);
+        num.add(976543);
+        num.add(44);
+        num.add(6245);
+        num.add(9);
+        num.add(3);
+        num.add(98);
+
+        System.out.println(hw.removeNegativeValue(num));
+        System.out.println(hw.getMax(num));
+        System.out.println(hw.getMin(num));
+        System.out.println(hw.getAverage(num));
     }
 
-    // Нужно удалить из него четные числа
-    public List<Integer> removeNegativeValue(List<Integer> list){
-
-        return new ArrayList<>();
-    }
-
-    // Найти минимальное значение
-    public Integer getMin(List<Integer> list){
-
-        return 0;
+    // Удалить четные числа
+    public List<Integer> removeNegativeValue(List<Integer> list) {
+        ArrayList<Integer> not_even = new ArrayList<>();
+        for (int i : list) {
+            if (i % 2 != 0) {
+                not_even.add(i);
+            }
+        }
+        return not_even;
     }
 
     // Найти максимальное значение
-    public Integer getMax(List<Integer> list){
+    public Integer getMax(List<Integer> list) {
 
-        return 0;
+        return Collections.max(list);
+    }
+
+    // Найти минимальное значение
+    public Integer getMin(List<Integer> list) {
+
+        return Collections.min(list);
     }
 
     // Найти среднее значение
-    public Integer getAverage(List<Integer> list){
-
-        return 0;
+    public Integer getAverage(List<Integer> list) {
+        Integer summ = list.stream().reduce(0, (a, b) -> a + b);
+        return summ / 2;
     }
 }
--- src/test/java/ru/gb/jseminar/HomeworkTest.java
+++ src/test/java/ru/gb/jseminar/HomeworkTest.java
@@ -1,5 +1,55 @@
 package ru.gb.jseminar;
 
-class HomeworkTest {
 
-}
+ import org.junit.jupiter.api.Assertions;
+ import org.junit.jupiter.api.Test;
+
+ import java.util.ArrayList;
+ import java.util.Arrays;
+ import java.util.List;
+
+ class HomeworkTest {
+
+     @Test
+     void getMaxSuccessful() {
+         List<Integer> expectedList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+         int expected = 5;
+
+         Homework hw = new Homework();
+         int actual = hw.getMax(expectedList);
+
+         Assertions.assertEquals(expected, actual);
+     }
+     @Test
+     void removeNegativeValueSuccessful() {
+         List<Integer> expectedList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+         List<Integer> expected = new ArrayList<>(Arrays.asList(1, 3, 5));
+
+         Homework hw = new Homework();
+         var actual = hw.removeNegativeValue(expectedList);
+
+         Assertions.assertArrayEquals(expected.toArray(), actual.toArray());
+     }
+     @Test
+     void getMinSuccessful() {
+         List<Integer> expectedList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+         int expected = 1;
+
+         Homework hw = new Homework();
+         int actual = hw.getMin(expectedList);
+
+         Assertions.assertEquals(expected, actual);
+     }
+     @Test
+     void getAverageSuccessful() {
+         List<Integer> expectedList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+         int expected = 7; // 7.5 if double
+
+         Homework hw = new Homework();
+         int actual = hw.getAverage(expectedList);
+
+         Assertions.assertEquals(expected, actual);
+     }
+
+
+ }
