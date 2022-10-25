551f59a1adf02fc9511927777d673ddbeb9ff7a2
Taisiya <lebei@mail.ru>
2022-10-25 18:25:29 +0300

    Hometest Done

--- src/test/java/ru/gb/jseminar/HomeworkTest.java
+++ src/test/java/ru/gb/jseminar/HomeworkTest.java
@@ -1,5 +1,45 @@
 package ru.gb.jseminar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+// commet
 class HomeworkTest {
+@Test
+void getMaxSuccessful() {
+    List<Integer> expectedList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+    int expected = 5;
+    Homework hw = new Homework();
+    int actual = hw.getMax(expectedList);
+    Assertions.assertEquals(expected, actual);
+}
+
+@Test
+void removeNegativeValueSuccessful() {
+    List<Integer> expectedList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+    List<Integer> expected = new ArrayList<>(Arrays.asList(1, 3, 5));
+    Homework hw = new Homework();
+    var actual = hw.removeNegativeValue(expectedList);
+    Assertions.assertArrayEquals(expected.toArray(), actual.toArray());
+}
+@Test
+void getMinSuccessful() {
+    List<Integer> expectedList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+    int expected = 1;
+    Homework hw = new Homework();
+    int actual = hw.getMin(expectedList);
+    Assertions.assertEquals(expected, actual);
+}
+@Test
+void getAverageSuccessful() {
+    List<Integer> expectedList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+    int expected = 7; 
+    Homework hw = new Homework();
+    int actual = hw.getAverage(expectedList);
+    Assertions.assertEquals(expected, actual);
+}
 
 }
