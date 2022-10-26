a1b1c1581c1f386cba43625f10dd2c264f211e95
Nikita <smiels@vk.com>
2022-10-26 18:56:03 +0300

    seminar 3 + hw

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,36 +1,47 @@
 package ru.gb.jseminar;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Homework {
 
     // Пусть дан произвольный список целых чисел
     public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(-10, -8, -2, 0, 2, 8, 10));
+        Homework hw = new Homework();
+        System.out.println(list);
+        System.out.println(hw.removeNegativeValue(list) + " << Remove negative");
+        System.out.println(hw.getMin(list) + " << Min value");
+        System.out.println(hw.getMax(list) + " << Max value");
+        System.out.println(hw.getAverage(list) + " << Avg value");
 
     }
 
     // Нужно удалить из него четные числа
     public List<Integer> removeNegativeValue(List<Integer> list){
-
-        return new ArrayList<>();
+        List<Integer> tempList = new ArrayList<>(list);
+        tempList.removeIf(integer -> integer < 0);
+        // идея предложила заменить итератор своим решением на основе итератора.
+        // А почему бы и нет?
+        return tempList;
     }
 
     // Найти минимальное значение
     public Integer getMin(List<Integer> list){
-
-        return 0;
+        return Collections.min(list);
     }
 
     // Найти максимальное значение
     public Integer getMax(List<Integer> list){
-
-        return 0;
+        return Collections.max(list);
     }
 
     // Найти среднее значение
     public Integer getAverage(List<Integer> list){
-
-        return 0;
+        int avg = 0;
+        for (int item: list) {
+            avg+=item;
+        }
+        avg /= list.size();
+        return avg;
     }
 }
--- src/main/java/ru/gb/jseminar/Task0.java
+++ src/main/java/ru/gb/jseminar/Task0.java
@@ -10,7 +10,14 @@ public class Task0 {
     // String s5 = new String("hello");
     // String s6 = new String(new char[]{'h', 'e', 'l', 'l', 'o'});
     public static void main(String[] args) {
+        String s1 = "hello";
+        String s2 = "Hello";
+        String s3 = s1;
+        String s4 = "h" + "e" + "l" + "l" + "o";
+        String s5 = new String("hello");
+        String s6 = new String(new char[]{'h', 'e', 'l', 'l', 'o'});
 
+        System.out.println(s1.equalsIgnoreCase(s2));
     }
 
 }
