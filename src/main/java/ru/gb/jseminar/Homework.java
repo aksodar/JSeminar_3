97817555007190fc65c6ee4c39458358c8b3a872
MaxLeontev <lmaks21@mail.ru>
2022-10-26 20:26:57 +0300

    add homework

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,36 +1,54 @@
 package ru.gb.jseminar;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Homework {
 
     // Пусть дан произвольный список целых чисел
     public static void main(String[] args) {
-
+        Homework homework= new Homework();
+        List<Integer>list = new ArrayList<>(Arrays.asList(2, 5, 3, 7, 9, 6, 1, 8));
+        List<Integer>list2=new ArrayList<>();
+        System.out.println(homework.removeNegativeValue(list));
+        System.out.println(homework.getMin(list));
+        System.out.println(homework.getMax(list));
+        System.out.println(homework.getAverage(list));
     }
 
     // Нужно удалить из него четные числа
     public List<Integer> removeNegativeValue(List<Integer> list){
-
-        return new ArrayList<>();
+        List<Integer>listrem=new ArrayList<>();
+        for (int i:list) {
+            if (i %2!=0){
+                listrem.add(i);
+            }
+        }
+        return listrem;
     }
 
     // Найти минимальное значение
     public Integer getMin(List<Integer> list){
-
-        return 0;
+        Integer min=Collections.min(list);
+        return min;
     }
 
     // Найти максимальное значение
     public Integer getMax(List<Integer> list){
-
-        return 0;
+        Integer max=Collections.max(list);
+        return max;
     }
 
     // Найти среднее значение
-    public Integer getAverage(List<Integer> list){
+    public double getAverage(List<Integer> list){
+        Integer sum=0;
+        for (int i: list) {
+            sum+=i;
+        }
+        double average=sum/list.size();
 
-        return 0;
+        return average;
     }
 }
--- src/main/java/ru/gb/jseminar/Task2.java
+++ src/main/java/ru/gb/jseminar/Task2.java
@@ -2,6 +2,7 @@ package ru.gb.jseminar;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class Task2 {
@@ -14,6 +15,16 @@ public class Task2 {
     }
 
     public static void printResultOfCheck(List<String> list){
+        Iterator <String> iter = list.iterator();
+        while (iter.hasNext()){
+            try{
+                Integer.valueOf(iter.next());
+                System.out.println("it's number");
+            }catch (NumberFormatException e){
+                System.out.println("it's string");
+            }
+
+        }
 
     }
 
