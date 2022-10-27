cf40025317f9a5060cbf49c3f4ed6f206a2b1cc1
LeaClover <plesen_14@mail.ru>
2022-10-27 19:15:20 +0300

    edit project homework

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,36 +1,49 @@
 package ru.gb.jseminar;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Homework {
 
     // Пусть дан произвольный список целых чисел
     public static void main(String[] args) {
-
+        Homework hw = new Homework();
+        List <Integer> numbers = new ArrayList <Integer> ();
+        Collections.addAll(numbers, 4, 5, 2, 1, 6, 8);
+        System.out.printf("Список: " + numbers);
+        System.out.println();
+        System.out.printf("Четные: " + hw.removeNegativeValue(numbers));
+        System.out.println();
+        System.out.printf("Минимальное значение: " + hw.getMin(numbers));
+        System.out.println();
+        System.out.printf("Максимальное значение: " + hw.getMax(numbers));
+        System.out.println();
+        System.out.printf("Ср. арифметическое: " + hw.getAverage(numbers));
     }
 
     // Нужно удалить из него четные числа
     public List<Integer> removeNegativeValue(List<Integer> list){
-
-        return new ArrayList<>();
+        ArrayList <Integer> evenList = new ArrayList<Integer>(list);
+        evenList.removeIf(x -> x % 2 == 1);
+        return new ArrayList<Integer>(evenList);
     }
 
     // Найти минимальное значение
     public Integer getMin(List<Integer> list){
-
-        return 0;
+        return Collections.min(list);
     }
 
     // Найти максимальное значение
     public Integer getMax(List<Integer> list){
-
-        return 0;
+        return Collections.max(list);
     }
 
     // Найти среднее значение
     public Integer getAverage(List<Integer> list){
-
-        return 0;
+        int length = list.size();
+        int sum = list.stream().mapToInt(a -> a).sum();
+        int average = sum / length;
+        return average;
     }
 }
