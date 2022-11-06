46e7ba5c1322a2f01bedbe4e4e8cc9f4462c2ee1
Oleg Osipov <vopisso@gmail.com>
2022-11-07 00:23:38 +0300

    Tasks are done at seminar

--- src/main/java/ru/gb/jseminar/Task0.java
+++ src/main/java/ru/gb/jseminar/Task0.java
@@ -10,7 +10,26 @@ public class Task0 {
     // String s5 = new String("hello");
     // String s6 = new String(new char[]{'h', 'e', 'l', 'l', 'o'});
     public static void main(String[] args) {
+         String s1 = "hello";
+         String s2 = "hello";
+         String s3 = s1;
+         String s4 = "h" + "e" + "l" + "l" + "o";
+         String s5 = new String("hello");
+         String s6 = new String(new char[]{'h', 'e', 'l', 'l', 'o'});
 
+        System.out.println("s1 == s2: " + (s1 == s2));
+        System.out.println("s1 == s3: " + (s1 == s3));
+        System.out.println("s1 == s4: " + (s1 == s4));
+        System.out.println("s1 == s5: " + (s1 == s5));
+        System.out.println("s2 == s3: " + (s2 == s3));
+
+        System.out.println("s2 == s5: " + (s2 == s5));
+        System.out.println("s3 == s4: " + (s3 == s4));
+
+        System.out.println();
+        System.out.println(s1.equalsIgnoreCase(s2));
+        System.out.println(s5.equalsIgnoreCase(s6));
+        System.out.println(s4.equalsIgnoreCase(s6));
     }
 
 }
--- src/main/java/ru/gb/jseminar/Task2.java
+++ src/main/java/ru/gb/jseminar/Task2.java
@@ -2,6 +2,7 @@ package ru.gb.jseminar;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class Task2 {
@@ -9,11 +10,22 @@ public class Task2 {
     // Дан список содержащий строки и числа в строковом формате.
     // C помощью итератора пройти по списку и вывести в консоль что является строкой, а что числом
     public static void main(String[] args) {
+        Task2 t2 = new Task2();
         List<String> list = new ArrayList<>(Arrays.asList("a", "2", "b", "3", "c", "4"));
-        printResultOfCheck(list);
+        t2.printResultOfCheck(list);
     }
 
-    public static void printResultOfCheck(List<String> list){
+    public void printResultOfCheck(List<String> list) {
+        Iterator<String> iter = list.iterator();
+        while (iter.hasNext()) {
+            try {
+                int a = Integer.valueOf(iter.next());
+                System.out.println(a + " It's a number");
+            } catch (NumberFormatException e) {
+                System.out.println(" It's a string");
+            }
+
+        }
 
     }
 
