db273f34ddcc9eafd585fc3c59eba38c9ad94a56
Leo_Amv <leonidamvrosov@yandex.ru>
2022-11-09 23:09:06 +0500

    Homework completed!

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -2,36 +2,63 @@ package ru.gb.jseminar;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class Homework {
 
     // Пусть дан произвольный список целых чисел
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(Arrays.asList(1, 9, 9, 6, 0, 3, 0, 4, 2, 3, 0, 8));
+        Homework hw = new Homework();
+        Logger log = Logger.getLogger(Homework.class.getName());
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 9, 9, 6, 0, 3, 0, 4, 2, 3, 0, 8, -5, -3, -10));
+        log.info("Cписок целых чисел:\t" + list.toString());
+        log.info("Удалить из него четные числа:\t" + hw.removeNegativeValue(list).toString());
+        log.info("Минимальное значение:\t" + hw.getMin(list).toString());
+        log.info("Максимальное значение:\t" + hw.getMax(list).toString());
+        log.info("Среднее значение:\t" + hw.getAverage(list).toString());
     }
 
     // Нужно удалить из него четные числа
     public List<Integer> removeNegativeValue(List<Integer> list) {
-
-        return new ArrayList<>();
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next() % 2 == 0) {
+                iterator.remove();
+            }
+        }
+        return list;
     }
 
     // Найти минимальное значение
     public Integer getMin(List<Integer> list) {
-
-        return 0;
+        int min = list.get(0);
+        for (int number : list) {
+            if (number < min) {
+                min = number;
+            }
+        }
+        return min;
     }
 
     // Найти максимальное значение
     public Integer getMax(List<Integer> list) {
-
-        return 0;
+        int max = list.get(0);
+        for (int number : list) {
+            if (number > max) {
+                max = number;
+            }
+        }
+        return max;
     }
 
     // Найти среднее значение
     public Integer getAverage(List<Integer> list) {
-
-        return 0;
+        int sum = 0;
+        for (int number : list) {
+            sum += number;
+        }
+        return sum / list.size();
     }
 }
