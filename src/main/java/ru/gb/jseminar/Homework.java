bd912d6e775aad6b64760aff869bff5c169b69bb
Framuga93 <alekseydemochkin93@gmail.com>
2022-11-10 17:57:01 +0300

    add hw seminar 3

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,36 +1,48 @@
 package ru.gb.jseminar;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Homework {
 
     // Пусть дан произвольный список целых чисел
     public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(2,3,4,5)) ;
+        Homework hw = new Homework();
+        System.out.println(hw.getMax(list));
+        System.out.println(hw.getMin(list));
 
     }
 
     // Нужно удалить из него четные числа
     public List<Integer> removeNegativeValue(List<Integer> list){
+            Iterator<Integer> iterator = list.iterator();
+            while (iterator.hasNext()) {
+                if (iterator.next() % 2 == 0) {
+                    iterator.remove();
+                }
+            }
+            return list;
+        }
 
-        return new ArrayList<>();
-    }
 
     // Найти минимальное значение
     public Integer getMin(List<Integer> list){
-
-        return 0;
+        int min = Collections.min(list);
+        return min;
     }
 
     // Найти максимальное значение
     public Integer getMax(List<Integer> list){
-
-        return 0;
+        int max = Collections.max(list);
+        return max;
     }
 
     // Найти среднее значение
-    public Integer getAverage(List<Integer> list){
-
-        return 0;
+    public Double getAverage(List<Integer> list){
+        double sum = 0;
+        for (int item : list){
+            sum += item;
+        }
+        return sum/list.size();
     }
 }
--- src/test/java/ru/gb/jseminar/HomeworkTest.java
+++ src/test/java/ru/gb/jseminar/HomeworkTest.java
@@ -1,5 +1,57 @@
 package ru.gb.jseminar;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.Inet4Address;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class HomeworkTest {
 
+    @Test
+    void removeNegativeValue() {
+        List<Integer> expectedList = new ArrayList<>(Arrays.asList(1,2,3,4));
+        List<Integer> expected = new ArrayList<>(Arrays.asList(1,3));
+
+        Homework hw = new Homework();
+        List<Integer> actual = hw.removeNegativeValue(expectedList);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    void getMinSuccesfull() {
+        List<Integer> expectedList = new ArrayList<>(Arrays.asList(1,2,3,4));
+        int expected = 1;
+
+        Homework hw = new Homework();
+        int actual = hw.getMin(expectedList);
+
+        Assertions.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    void getMaxSuccesfull() {
+        List<Integer> expectedList = new ArrayList<>(Arrays.asList(1,2,3,4));
+        int expected = 4;
+
+        Homework hw = new Homework();
+        int actual = hw.getMax(expectedList);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    void getAverage() {
+        List<Integer> expectedList = new ArrayList<>(Arrays.asList(1,2,3,4));
+        double expected = 2.5 ;
+
+        Homework hw = new Homework();
+        double actual = hw.getAverage(expectedList);
+
+        Assertions.assertEquals(expected,actual);
+    }
 }
