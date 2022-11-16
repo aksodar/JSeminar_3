129c40754f8a4c1039dfff8ec70df54cfce8364b
AlexServGE <alexander.sergeevru@yandex.ru>
2022-11-16 07:56:08 +0300

    Please validate Homework3 and choose your mark

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,36 +1,65 @@
 package ru.gb.jseminar;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Homework {
 
     // Пусть дан произвольный список целых чисел
     public static void main(String[] args) {
-
+        Homework task1 = new Homework();
+        List<Integer> taskList = new ArrayList<>(List.of(task1.arrayRndIntegers(2)));
+        System.out.println(taskList);
+        //Метод removeNegativeValue
+        List<Integer> taskList1 = new ArrayList<>(taskList);
+        task1.removeNegativeValue(taskList1);
+        System.out.println(taskList1);
+        //Метод getMin
+        List<Integer> taskList2 = new ArrayList<>(taskList);
+        System.out.println(task1.getMin(taskList2));
+        //Метод getMax
+        List<Integer> taskList3 = new ArrayList<>(taskList);
+        System.out.println(task1.getMax(taskList3));
+        //Метод getAverage
+        List<Integer> taskList4 = new ArrayList<>(taskList);
+        System.out.println(task1.getAverage(taskList4));
     }
-
+    public Integer[] arrayRndIntegers(int size) {
+        Integer[] someArray = new Integer[size];
+        Random rnd = new Random();
+        for (int i = 0; i < size; i++) {
+            int flag = rnd.nextInt(2) == 0 ? -1 : 1;
+            someArray[i] = rnd.nextInt(101) * flag;
+        }
+        return someArray;
+    }
     // Нужно удалить из него четные числа
-    public List<Integer> removeNegativeValue(List<Integer> list){
-
-        return new ArrayList<>();
+    public List<Integer> removeNegativeValue(List<Integer> list) {
+        Iterator<Integer> i = list.iterator();
+        while (i.hasNext()) {
+            if (i.next() < 0) i.remove();
+        }
+        return list;
     }
 
     // Найти минимальное значение
-    public Integer getMin(List<Integer> list){
-
-        return 0;
+    public Integer getMin(List<Integer> list) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(list);
+        return queue.peek();
     }
 
     // Найти максимальное значение
-    public Integer getMax(List<Integer> list){
-
-        return 0;
+    public Integer getMax(List<Integer> list) {
+//        Collections.max(list);
+        list.sort(Comparator.reverseOrder());
+        return list.get(0);
     }
 
     // Найти среднее значение
-    public Integer getAverage(List<Integer> list){
-
-        return 0;
+    public Double getAverage(List<Integer> list) {
+        int sum = 0;
+        for (int item : list) {
+            sum += item;
+        }
+        return sum / (double) list.size();
     }
 }
--- src/test/java/ru/gb/jseminar/HomeworkTest.java
+++ src/test/java/ru/gb/jseminar/HomeworkTest.java
@@ -1,5 +1,36 @@
 package ru.gb.jseminar;
 
-class HomeworkTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+class HomeworkTest {
+    Homework tk = new Homework();
+    List<Integer> initialList;
+
+    @Test
+    void removeNegativeValueText() {
+        initialList = new ArrayList<>(List.of(-1, -2, 3, 4));
+        Assertions.assertEquals(List.of(3, 4), tk.removeNegativeValue(initialList));
+    }
+
+    @Test
+    void getMinText() {
+        initialList = new ArrayList<>(List.of(-1, -2, 3, 4));
+        Assertions.assertEquals(-2, tk.getMin(initialList));
+    }
+
+    @Test
+    void getMaxText() {
+        initialList = new ArrayList<>(List.of(-1, -2, 3, 4));
+        Assertions.assertEquals(4, tk.getMax(initialList));
+    }
+
+    @Test
+    void getAverageText() {
+        initialList = new ArrayList<>(List.of(3, 4));
+        Assertions.assertEquals(3.5, tk.getAverage(initialList));
+    }
 }
