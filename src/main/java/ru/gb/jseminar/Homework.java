16f037bf6424507301ed0b6f3745e6015a476cb1
Aleksander Sinkov <silwa2@yandex.ru>
2022-12-14 15:46:17 +0300

    Домашнее задание семинар 3

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,36 +1,58 @@
 package ru.gb.jseminar;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Homework {
 
+
     // Пусть дан произвольный список целых чисел
     public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 9, 2, 4, 25, -8, 98));
+        System.out.println("The desired array: " + list);
 
+        Homework numbers = new Homework();
+        System.out.println("The resulting array: " + numbers.removeNegativeValue(list));
+
+        numbers.getMin(list);
+
+        numbers.getMax(list);
+
+        System.out.println("average amount: " + numbers.getAverage(list));
     }
 
     // Нужно удалить из него четные числа
-    public List<Integer> removeNegativeValue(List<Integer> list){
-
-        return new ArrayList<>();
+    public List<Integer> removeNegativeValue(List<Integer> list) {
+        List<Integer> list2 = new ArrayList<>();
+        for (int i : list) {
+            if (i % 2 != 0) {
+                list2.add(i);
+            }
+        }
+        return list2;
     }
 
     // Найти минимальное значение
     public Integer getMin(List<Integer> list){
-
+        System.out.println("Min Element: " + Collections.min(list));
         return 0;
     }
 
+
     // Найти максимальное значение
     public Integer getMax(List<Integer> list){
-
+        System.out.println("Max Element: " + Collections.max(list));
         return 0;
     }
 
     // Найти среднее значение
-    public Integer getAverage(List<Integer> list){
-
-        return 0;
+    public Double getAverage(List<Integer> list) {
+        long sum = 0;
+        for (int i : list) {
+            sum += i;
+        }
+        return list.size() > 0 ? (double) sum / list.size() : 0;
     }
 }
--- src/main/java/ru/gb/jseminar/Task2.java
+++ src/main/java/ru/gb/jseminar/Task2.java
@@ -9,12 +9,18 @@ public class Task2 {
     // Дан список содержащий строки и числа в строковом формате.
     // C помощью итератора пройти по списку и вывести в консоль что является строкой, а что числом
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>(Arrays.asList("a", "2", "b", "3", "c", "4"));
+        List<String> list = new ArrayList<>(Arrays.asList("a", "2", "b", "3", "c", "4", "67"));
         printResultOfCheck(list);
     }
 
-    public static void printResultOfCheck(List<String> list){
-
+    public static void printResultOfCheck(List<String> list) {
+        for (String s : list) {
+            try {
+                Integer.valueOf(s);
+                System.out.println(s + " - Integer");
+            } catch (Exception e) {
+                System.out.println(s + " - String");
+            }
+        }
     }
-
 }
