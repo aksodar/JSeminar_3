b7d7b5166e97bd0f9eade4840221dffde694e4d1
Darina <krastinadv@gmail.com>
2022-12-16 20:04:15 +0300

    add homework

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,36 +1,63 @@
 package ru.gb.jseminar;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+
+
 
 public class Homework {
 
-    // Пусть дан произвольный список целых чисел
     public static void main(String[] args) {
+        Homework hw = new Homework();
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for (int i = 0; i < 10; i++) {
+            Random rnd = new Random();
+            int val = rnd.nextInt(100);
+            list.add(val);
+        }
+        System.out.println("Произвольный список чисел : " + list);
+        System.out.println("Список без четных чисел :"+ hw.removeNegativeValue(list));
+        System.out.println("Минимальное число :"+ hw.getMin(list));
+        System.out.println("Максимальное число :"+ hw.getMax(list));
+        System.out.println("Среднее значение : "+ hw.getAverage(list));
 
     }
 
-    // Нужно удалить из него четные числа
-    public List<Integer> removeNegativeValue(List<Integer> list){
+    public  List<Integer> removeNegativeValue(List<Integer> list){
+        for(int i = list.size() - 1; i > -1 ; i--){
+            if(list.get(i) % 2 == 0){
+                list.remove(i);
+            }
+        }
+        return list;}
 
-        return new ArrayList<>();
+
+    public  Integer getMin(List<Integer> list){
+        return Collections.min(list);
     }
 
-    // Найти минимальное значение
-    public Integer getMin(List<Integer> list){
-
-        return 0;
-    }
-
-    // Найти максимальное значение
     public Integer getMax(List<Integer> list){
-
-        return 0;
+        return Collections.max(list);
     }
 
-    // Найти среднее значение
-    public Integer getAverage(List<Integer> list){
 
-        return 0;
+    public Integer getAverage(List<Integer> list){
+        int average = 0;
+        if (list.size() > 0){
+            {
+                int sum = 0;
+                for (int item : list) {
+                    sum += item;
+                }
+                average = sum/list.size();
+            }
+
+        }
+        return average;
     }
 }
+
