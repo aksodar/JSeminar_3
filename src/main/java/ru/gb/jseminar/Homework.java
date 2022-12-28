1e1e4084470964f2292fe2af445bc6a25eff50b6
Max <imv-1max@yandex.ru>
2022-12-29 00:02:49 +0500

    add homework lesson3

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,36 +1,58 @@
 package ru.gb.jseminar;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class Homework {
 
     // Пусть дан произвольный список целых чисел
     public static void main(String[] args) {
+        List <Integer> list = new ArrayList<>(Arrays.asList( 9, 2, 6, 4, 3, 5, 7, 8, 11));
+        int lst[] = {  9, 2, 6, 4, 3, 5, 7, 8, 11};
+        double sum = IntStream.of(lst).sum();
+        double z =(sum / lst.length);
+        System.out.printf("Average value: %.1f",z);
+        System.out.println();
 
+        Homework testObject = new Homework();
+
+        System.out.println("List of odd numbers: " + testObject.removeEvenValue(list));
+        System.out.println("Min value: " + testObject.getMin(list));
+        System.out.println("Max value: " + testObject.getMax(list));
+        System.out.println("Average of odd values: " + testObject.getAverage(list));
     }
 
     // Нужно удалить из него четные числа
-    public List<Integer> removeEvenValue(List<Integer> list){
-
-        return new ArrayList<>();
+    public  List<Integer> removeEvenValue(List<Integer> list) {
+        Iterator<Integer> it = list.iterator();
+        while (it.hasNext()) {
+            Integer number = it.next();
+            if (number % 2 == 0) {
+                it.remove();
+            }
+        }
+        return list;
     }
 
-    // Найти минимальное значение
+    //    Найти минимальное значение
     public Integer getMin(List<Integer> list){
-
-        return 0;
+        int m = Collections.min(list);
+        return m;
     }
 
     // Найти максимальное значение
     public Integer getMax(List<Integer> list){
-
-        return 0;
+        int m = Collections.max(list);
+        return m;
     }
 
     // Найти среднее значение
-    public Integer getAverage(List<Integer> list){
+    public double getAverage(List<Integer> list){
+        float sum = 0;
+        for(int s : list){
+            sum+=s;
+        }
 
-        return 0;
+        return  sum / list.size();
     }
 }
